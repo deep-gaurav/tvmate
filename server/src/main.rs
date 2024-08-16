ebcc135d3a2005c8fc96ee4bf615f0841aded796
Deep <deepgauravraj@gmail.com>
2024-08-16 13:13:41 +0530

    add compression layer

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,7 @@ use axum::Router;
 use fileserv::file_and_error_handler;
 use leptos::*;
 use leptos_axum::{generate_route_list, LeptosRoutes};
+use tower_http::compression::CompressionLayer;
 
 pub mod fileserv;
 
@@ -20,11 +21,14 @@ async fn main() {
     let addr = leptos_options.site_addr;
     let routes = generate_route_list(App);
 
+    let compression = CompressionLayer::new();
+
     // build our application with a route
     let app = Router::new()
         .leptos_routes(&leptos_options, routes, App)
         .fallback(file_and_error_handler)
-        .with_state(leptos_options);
+        .with_state(leptos_options)
+        .layer(compression);
 
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
