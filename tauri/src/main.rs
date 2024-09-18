8528dc3b648950ede95f29b4c55a06d96cd6f3f1
Deep <deepgauravraj@gmail.com>
2024-09-18 19:42:46 +0530

    use tvmate.deepgaurav.com

--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -4,7 +4,7 @@ use leptos::*;
 fn main() {
     console_error_panic_hook::set_once();
     let endpoint = Endpoint {
-        main_endpoint: std::borrow::Cow::Borrowed("wss://synced-crt.deepwith.in"),
+        main_endpoint: std::borrow::Cow::Borrowed("wss://tvmate.deepgaurav.com"),
     };
     mount_to_body(|| {
         provide_context(endpoint);
